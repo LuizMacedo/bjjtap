940e5d6347e0e75e9e7ab6d6ffe663b8bb8b51fe
Luiz Macedo <45747223+LuizMacedo@users.noreply.github.com>
2024-12-10 02:12:36 +0000

    update rejectunauthorized to false

--- infra/database.js
+++ infra/database.js
@@ -32,7 +32,7 @@ function getSSLValue() {
   if (process.env.POSTGRES_CA) {
     return {
       ca: process.env.POSTGRES_CA,
-      rejectUnauthorized: true,
+      rejectUnauthorized: false,
     };
   }
   return process.env.NODE_ENV === "development" ? false : true;
