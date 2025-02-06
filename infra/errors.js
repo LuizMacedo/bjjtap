0619995863828580987fd0d713d3dfa37fe19ac1
Luiz Macedo <45747223+LuizMacedo@users.noreply.github.com>
2025-02-06 03:16:06 +0000

    feat: add `MethodNotAllowedError` and `ServiceError`

--- infra/errors.js
+++ infra/errors.js
@@ -1,12 +1,12 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("Unexpected Internal Server Error.", {
       cause,
     });
 
     this.name = "InternalServerError";
     this.action = "Please contact our support team for assistance.";
-    this.status = 500;
+    this.statusCode = statusCode || 500;
   }
 
   toJSON() {
@@ -14,7 +14,46 @@ export class InternalServerError extends Error {
       name: this.name,
       message: this.message,
       action: this.action,
-      status_code: this.status,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Service unavailable.", {
+      cause,
+    });
+
+    this.name = "ServiceError";
+    this.action = "Please verify if the service is available.";
+    this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Method Not Allowed");
+    this.name = "MethodNotAllowedError";
+    this.action = "Verify if the HTTP method is correct";
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
     };
   }
 }
