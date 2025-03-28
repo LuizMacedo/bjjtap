1e13398d206a50cedf49e7ec585734b6bc81e545
Luiz Macedo <45747223+LuizMacedo@users.noreply.github.com>
2025-03-28 04:52:48 +0000

    feat: create and use `NotFoundError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -61,6 +61,27 @@ export class ValidationError extends Error {
   }
 }
 
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Resource not found.", {
+      cause,
+    });
+
+    this.name = "NotFoundError";
+    this.action = action || "Please check the resource and try again.";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
 export class MethodNotAllowedError extends Error {
   constructor() {
     super("Method Not Allowed");
